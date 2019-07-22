47668755e8af3ef2e48dc4ee10bb2c0cb30335f3
Jeshvina <52188942+Jeshvina@users.noreply.github.com>
2019-07-23 00:06:06 +0530

    ei.py

---
+++ ei.py
@@ -0,0 +1,24 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=a-c
+f=c-a
+k=b-d
+j=d-b
+if(a>c):
+    {
+     print(e,end=" ")  
+       }
+else:
+    {
+        print(f,end=" ")
+       }
+if(b>d):
+ {
+    print(k)
+    }
+else:
+    {
+        print(j)
+        }
